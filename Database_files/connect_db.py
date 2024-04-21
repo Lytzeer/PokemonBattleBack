0f5142c1b036328759481e1f4f46fe8dbab537a5
Lytzeer <lukasportier@gmail.com>
2024-04-21 21:45:07 +0200

    Api -> Fix Api Route

--- Database_files/connect_db.py
+++ Database_files/connect_db.py
@@ -1,7 +1,7 @@
 import mariadb
 import sys
 from database_getter import get_pokemon,get_pokemon_by_id, get_move_by_id, get_pokemon_types_by_id, get_pokemon_moves, get_pokemon_types, get_pokemon_stats, get_pokedex_id_by_name, get_pokedex_id_by_pokemon_id, get_pokemon_id_by_name, get_type_by_id, get_type, get_move, get_pokemon_moves_by_id
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -28,34 +28,44 @@ def connect():
 
 cur = connect()
 
+@app.route('/pokemon/')
+@app.route('/')
+def home():
+    return redirect('/pokemon', code=302)
+
 @app.route('/pokemon')
 def list_pokemon():
     pokemon=get_pokemon(cur)
     print(pokemon[0])
-    return jsonify(pokemon)
+    return jsonify(pokemon), 200
 
 @app.route('/pokemon/<int:pokemon_id>')
+@app.route('/pokemon/<int:pokemon_id>/')
 def list_pokemon_by_id(pokemon_id):
     pokemon=get_pokemon_by_id(cur, pokemon_id)
-    return jsonify(pokemon)
+    return jsonify(pokemon), 200
 
 @app.route('/move')
+@app.route('/move/')
 def list_move():
     move=get_move(cur)
-    return jsonify(move)
+    return jsonify(move), 200
 
 @app.route('/move/<int:move_id>')
+@app.route('/move/<int:move_id>/')
 def list_move_by_id(move_id):
     move=get_move_by_id(cur, move_id)
-    return jsonify(move)
+    return jsonify(move), 200
 
 @app.route('/pokemon/<int:pokemon_id>/move')
+@app.route('/pokemon/<int:pokemon_id>/move/')
 def list_pokemon_moves(pokemon_id):
-    return jsonify(get_pokemon_moves_by_id(cur, pokemon_id))
+    return jsonify(get_pokemon_moves_by_id(cur, pokemon_id)), 200
 
 @app.route('/pokemon/<int:pokemon_id>/type')
+@app.route('/pokemon/<int:pokemon_id>/type/')
 def list_pokemon_types(pokemon_id):
-    return jsonify(get_pokemon_types_by_id(cur, pokemon_id))
+    return jsonify(get_pokemon_types_by_id(cur, pokemon_id)), 200
 
 if __name__ == '__main__':
     app.run()
