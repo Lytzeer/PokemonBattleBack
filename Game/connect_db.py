492c81ae5c4867b2cd6e78f5d41d35a297a5c812
Lytzeer <lukasportier@gmail.com>
2024-04-24 14:04:35 +0200

    Api -> Add User Info Route For Home Page

--- Game/connect_db.py
+++ Game/connect_db.py
@@ -1,6 +1,6 @@
 import mariadb
 import sys
-from database_getter import get_pokemon,get_pokemon_by_id, get_move_by_id, get_pokemon_types_by_id, get_pokemon_moves, get_pokemon_types, get_pokemon_stats, get_pokedex_id_by_name, get_pokedex_id_by_pokemon_id, get_pokemon_id_by_name, get_type_by_id, get_type, get_move, get_pokemon_moves_by_id
+from database_getter import get_pokemon,get_pokemon_by_id, get_move_by_id, get_pokemon_types_by_id, get_move, get_pokemon_moves_by_id, get_user_pokemon, get_user_info
 from flask import Flask, jsonify, redirect
 from flask_cors import CORS
 
@@ -28,12 +28,12 @@ def connect():
 
 cur = connect()
 
-@app.route('/pokemon/')
 @app.route('/')
 def home():
     return redirect('/pokemon', code=302)
 
 @app.route('/pokemon')
+@app.route('/pokemon/')
 def list_pokemon():
     pokemon=get_pokemon(cur)
     return jsonify(pokemon), 200
@@ -65,6 +65,21 @@ def list_pokemon_moves(pokemon_id):
 @app.route('/pokemon/<int:pokemon_id>/type/')
 def list_pokemon_types(pokemon_id):
     return jsonify(get_pokemon_types_by_id(cur, pokemon_id)), 200
+
+@app.route('/user_pokemon/<int:user_id>')
+@app.route('/user_pokemon/<int:user_id>/')
+def list_user_pokemon(user_id):
+    return jsonify(get_user_pokemon(cur, user_id)), 200
+
+@app.route('/user_info/<string:username>')
+@app.route('/user_info/<string:username>/')
+def list_user_info(username):
+    data = get_user_info(cur, username)
+    if data == None:
+        return jsonify({"error": "User not found"}), 404
+    else:
+        return {"last_pokemon": data[0], "money": data[1], "standing_position": data[2]}
+    
 
 if __name__ == '__main__':
     app.run()
--- Game/database_getter.py
+++ Game/database_getter.py
@@ -108,3 +108,56 @@ def get_pokedex_id_by_pokemon_id(cur, pokemon_id):
     for (pokedex_id, name) in cur:
         print(f"Pokedex ID: {pokedex_id}, Name: {name}")
         return {"pokedex_id": pokedex_id, "name": name}
+    
+def get_user_pokemon(cur, user_id):
+    cur.execute("SELECT user_id, pokemon_id FROM user_pokemon WHERE user_id = ?", (user_id,))
+    user_pokemon = []
+    for (user_id, pokemon_id) in cur:
+        user_pokemon.append({"user_id": user_id, "pokemon_id": pokemon_id})
+    return user_pokemon
+
+def get_last_user_pokemon(cur, user_id):
+    user_pokemon = get_user_pokemon(cur, user_id)
+    last_pokemons = []
+    last_pokemons.append(user_pokemon[-1])
+    last_pokemons.append(user_pokemon[-2])
+    return last_pokemons
+
+def get_user_info(cur, username):
+    cur.execute("SELECT user_id FROM users WHERE username = ?", (username,))
+    user_id = cur.fetchone()
+    info = []
+    info.append(get_last_user_pokemon(cur, user_id[0]))
+    cur.execute("SELECT money FROM users WHERE user_id = ?", (user_id[0],))
+    money = cur.fetchone()
+    info.append(money[0])
+    standing_position = get_standing_index(cur, username)
+    info.append(standing_position)
+    return info
+
+def get_user_id(cur, username):
+    cur.execute("SELECT user_id FROM users WHERE username = ?", (username,))
+    user_id = cur.fetchone()
+    return user_id[0]
+
+def get_all_users_info(cur):
+    cur.execute("SELECT user_id, money FROM users")
+    all_users = []
+    for (user_id, money) in cur:
+        all_users.append({"user_id": user_id, "money": money})
+    return all_users
+
+def find_index_user(users, user_id):
+    for i in range(len(users)):
+        if users[i]["user_id"] == user_id:
+            return i+1
+    return -1
+
+def get_standing_index(cur, username):
+    user_id = get_user_id(cur, username)
+    all_users = get_all_users_info(cur)
+    index = find_index_user(all_users, user_id)
+    if index == -1:
+        return {"error": "User not found"}
+    else:
+        return index
