e29097579b062ae7c45c5135031538cada434c43
Lytzeer <lukasportier@gmail.com>
2024-05-03 11:46:31 +0200

    Api -> Update Shop Route

--- Game/Api_func/user_setter.py
+++ Game/Api_func/user_setter.py
@@ -6,6 +6,7 @@ def set_money(cur, username, money):
     user_id = get_user_id(cur, username)
     current_money = get_money(cur, username)
     cur.execute("UPDATE users SET money = %s WHERE user_id = %s", (current_money-money, user_id))
+    cur.commit()
     return {"message": "Money updated successfully", "username": username, "money": current_money-money}
 
 def set_user_buy_items(cur, username, amount, article, price):
@@ -14,8 +15,10 @@ def set_user_buy_items(cur, username, amount, article, price):
         set_money(cur, username, amount*price)
         pokemon=get_random_pokemon(cur)
         cur.execute("INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (%s, %s)", (user_id, pokemon["pokemon_id"]))
+        cur.commit()
         return {"message": "Egg bought successfully", "article": article, "amount": amount, "price": amount*price, "pokemon": pokemon["name"]}
     pokeball = get_pokeball_by_name(cur, article)
     set_money(cur, username, amount*price)
     cur.execute("INSERT INTO user_pokeball (user_id, pokeball_id, amount) VALUES (%s, %s, %s)", (user_id, pokeball["pokeball_id"], amount))
+    cur.commit()
     return {"message": "Pokeball bought successfully", "article": article, "amount": amount, "price": amount*price}
