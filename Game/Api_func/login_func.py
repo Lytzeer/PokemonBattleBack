6c514cc7647a9005d7c0ef98009fcb6095a7fecc
Lytzeer <lukasportier@gmail.com>
2024-05-08 21:02:28 +0200

    CRUD -> Update

--- Game/Api_func/login_func.py
+++ Game/Api_func/login_func.py
@@ -1,4 +1,5 @@
 import bcrypt
+import json
 
 def check_login(cur, username, password):
     cur.execute("SELECT * FROM users WHERE username = ? ", (username,))
@@ -6,8 +7,9 @@ def check_login(cur, username, password):
     if user == None:
         return {"message": False}
     else:
+        salt = get_salt()
         bytes_password = password.encode('utf-8')
-        hash = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
+        hash = bcrypt.hashpw(bytes_password, salt)
         if bcrypt.checkpw(user[2].encode('utf-8'), hash):
             return {"message": True}
         else:
@@ -22,8 +24,14 @@ def register_user(cur, username, password, password2, email):
         if user != None:
             return {"message": False, "error": "Username already exists"}
         else:
+            salt = get_salt()
             bytes_password = password.encode('utf-8')
-            hash = bcrypt.hashpw(bytes_password, bcrypt.gensalt())
+            hash = bcrypt.hashpw(bytes_password, salt)
             cur.execute("INSERT INTO users (username, password, email, money) VALUES (?, ?, ?, 0)", (username, hash, email))
             cur.connection.commit()
             return {"message": True}
+        
+def get_salt():
+    with open('./Game/Config/api_conf.json', 'r') as file:
+        data = json.load(file)
+    return data["seed"]
