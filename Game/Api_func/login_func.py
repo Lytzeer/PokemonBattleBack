df469fe1f5e51421573c7e5e8601e211cf82fd73
Lytzeer <lukasportier@gmail.com>
2024-05-08 21:05:58 +0200

    CRUD -> Update

--- Game/Api_func/login_func.py
+++ Game/Api_func/login_func.py
@@ -7,7 +7,7 @@ def check_login(cur, username, password):
     if user == None:
         return {"message": False}
     else:
-        salt = get_salt()
+        salt = get_salt().encode('utf-8')
         bytes_password = password.encode('utf-8')
         hash = bcrypt.hashpw(bytes_password, salt)
         if bcrypt.checkpw(user[2].encode('utf-8'), hash):
@@ -24,7 +24,7 @@ def register_user(cur, username, password, password2, email):
         if user != None:
             return {"message": False, "error": "Username already exists"}
         else:
-            salt = get_salt()
+            salt = get_salt().encode('utf-8')
             bytes_password = password.encode('utf-8')
             hash = bcrypt.hashpw(bytes_password, salt)
             cur.execute("INSERT INTO users (username, password, email, money) VALUES (?, ?, ?, 0)", (username, hash, email))
