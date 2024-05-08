eb343b0d9fbb97c44111da553aa8b0ebee4b4727
Lytzeer <lukasportier@gmail.com>
2024-05-08 21:32:27 +0200

    User -> Add Starter Pokemon

--- Game/Api_func/login_func.py
+++ Game/Api_func/login_func.py
@@ -1,5 +1,8 @@
 import bcrypt
 import json
+from random import randint
+from users_getter import get_user_id
+from pokemon_getter import get_pokemon
 
 def check_login(cur, username, password):
     cur.execute("SELECT * FROM users WHERE username = ? ", (username,))
@@ -29,9 +32,23 @@ def register_user(cur, username, password, password2, email):
             hash = bcrypt.hashpw(bytes_password, salt)
             cur.execute("INSERT INTO users (username, password, email, money) VALUES (?, ?, ?, 0)", (username, hash, email))
             cur.connection.commit()
+            user_id = get_user_id(cur, username)
+            pokemon = choose_pokemon()
+            for i in range(4):
+                cur.execute("INSERT INTO user_pokemon (user_id, pokemon_id) VALUES (?,?)", (user_id,pokemon[i]['pokemon_id']))
+            cur.connection.commit()
             return {"message": True}
         
 def get_salt():
     with open('./Game/Config/api_conf.json', 'r') as file:
         data = json.load(file)
     return data["seed"]
+
+def choose_pokemon():
+    pokemon = get_pokemon()
+    pokemon_list = []
+    for i in range(4):
+        rnb = randint(0, len(pokemon)-1)
+        print(f"{i+1}: {pokemon[rnb]['name']}, ID: {pokemon[rnb]['pokemon_id']}")
+        pokemon_list.append(pokemon[rnb])
+    return pokemon_list
